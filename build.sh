2a245121f865625037ce16bd1ad7014c7f891852
Rob Beal <296433+robertbeal@users.noreply.github.com>
2019-10-01 20:23:52 +0100

    Fix arm builds

--- build.sh
+++ build.sh
@@ -7,8 +7,7 @@ arch="$1"
 case "$arch" in
 amd64) base_image="balenalib/amd64-alpine:latest" ;;
 i386) base_image="balenalib/i386-alpine:latest" ;;
-arm) base_image="balenalib/armv7hf-alpine:latest" ;;
-aarch64) base_image="balenalib/aarch64-alpine:latest" ;;
+armv7) base_image="balenalib/armv7hf-alpine:latest" ;;
 esac
 
 sed "1cFROM $base_image" Dockerfile >"Dockerfile.$arch"
