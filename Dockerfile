8b25f1093ff1772449a2a20ad538e8354150ce86
Rob Beal <rob@kohi.uk>
2020-10-01 00:32:17 +0100

    Improve image tests

--- Dockerfile
+++ Dockerfile
@@ -22,13 +22,13 @@ ARG UID=801
 ARG GID=801
 
 LABEL maintainer="github.com/robertbeal" \
-      org.label-schema.name="Gogs" \
-      org.label-schema.description="Enhanced Docker image for Gogs" \
-      org.label-schema.url="https://github.com/gogs/gogs" \
-      org.label-schema.version=$VERSION \
-      org.label-schema.vcs-ref=$COMMIT_ID \
-      org.label-schema.vcs-url="https://github.com/robertbeal/docker-gogs" \
-      org.label-schema.schema-version="1.0"
+  org.label-schema.name="Gogs" \
+  org.label-schema.description="Enhanced Docker image for Gogs" \
+  org.label-schema.url="https://github.com/gogs/gogs" \
+  org.label-schema.version=$VERSION \
+  org.label-schema.vcs-ref=$COMMIT_ID \
+  org.label-schema.vcs-url="https://github.com/robertbeal/docker-gogs" \
+  org.label-schema.schema-version="1.0"
 
 WORKDIR /app
 COPY --from=builder /gopath/src/github.com/gogs/gogs/gogs .
@@ -39,24 +39,24 @@ COPY src /etc
 
 # hadolint ignore=DL3018,DL4006
 RUN apk add --no-cache --repository=http://dl-cdn.alpinelinux.org/alpine/edge/testing/ \
-    bash \
-    ca-certificates \
-    git \
-    linux-pam \
-    openssh \
-    shadow \
-    s6-overlay \
-    && addgroup -g $GID git \
-    && adduser -s /bin/bash -D -h /data -u $UID -G git git \
-    && usermod -p '*' git \
-    && passwd -u git \
-    && mkdir -p ./log \
-    && chown -R git:git . \
-    && chmod -R 550 . \
-    && chmod 770  log
+  bash \
+  ca-certificates \
+  git \
+  linux-pam \
+  openssh \
+  shadow \
+  s6-overlay \
+  && addgroup -g $GID git \
+  && adduser -s /bin/bash -D -h /data -u $UID -G git git \
+  && usermod -p '*' git \
+  && passwd -u git \
+  && mkdir -p ./log \
+  && chown -R git:git . \
+  && chmod -R 550 . \
+  && chmod 770  log
 
 HEALTHCHECK --interval=30s --retries=3 CMD wget --spider http://localhost:3000/healthcheck || exit 1
 VOLUME /config /data
-EXPOSE 22 3000
+EXPOSE 22222 3000
 
 ENTRYPOINT ["/init"]
--- tests/test_image.py
+++ tests/test_image.py
@@ -4,74 +4,101 @@ import subprocess
 import testinfra
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope="session")
 def host(request):
     # set the working directory to where the Dockerfile lives
     path = os.path.dirname(os.path.abspath(__file__)) + "/../"
 
-    subprocess.check_call(['docker', 'build', '-t', 'robertbeal/gogs', '.'], cwd=path)
-    id = subprocess.check_output(
-        ['docker', 'run', '--rm', '-d', 'robertbeal/gogs'], cwd=path).decode().strip()
+    subprocess.check_call(["docker", "build", "-t", "robertbeal/gogs", "."], cwd=path)
+    id = (
+        subprocess.check_output(
+            ["docker", "run", "--rm", "-d", "robertbeal/gogs"], cwd=path
+        )
+        .decode()
+        .strip()
+    )
 
     yield testinfra.get_host("docker://" + id)
 
-    subprocess.check_call(['docker', 'rm', '-f', id])
+    subprocess.check_call(["docker", "rm", "-f", id])
 
 
 def test_system(host):
-    assert host.system_info.distribution == 'alpine'
+    assert host.system_info.distribution == "alpine"
 
 
 def test_gogs_process(host):
-    assert host.process.get(
-        user='git').args == '/app/gogs web --config /config/app.ini'
+    assert host.process.get(user="git").args == "/app/gogs web --config /config/app.ini"
 
 
 def test_cron_process(host):
-    assert host.process.get(
-        user='root', comm='crond').args == '/usr/sbin/crond -fS'
+    assert host.process.get(user="root", comm="crond").args == "/usr/sbin/crond -fS"
 
 
-def test_version(host):
-    assert os.environ.get('VERSION', '0.12.2') in host.check_output("/app/gogs --version")
+def test_ssh_process(host):
+    assert host.process.get(user="root", comm="sshd")
 
 
-def test_port(host):
+def test_web_port(host):
     assert host.socket("tcp://0.0.0.0:3000").is_listening
 
 
+def test_ssh_port(host):
+    assert host.socket("tcp://0.0.0.0:22222").is_listening
+
+
 def test_user(host):
-    assert host.user('git').uid == 801
-    assert host.user('git').gid == 801
-    assert host.user('git').shell == '/bin/bash'
+    assert host.user("git").uid == 801
+    assert host.user("git").gid == 801
+    assert host.user("git").shell == "/bin/bash"
 
 
 def test_user_is_locked(host):
-    assert 'git L ' in host.check_output('passwd --status git')
+    assert "git L " in host.check_output("passwd --status git")
 
 
 def test_app_folder(host):
-    folder = '/app'
-    assert host.file(folder).exists
-    assert host.file(folder).user == 'git'
-    assert host.file(folder).group == 'git'
-    assert oct(host.file(folder).mode) == '0o550'
+    assert host.file("/app").exists
+    assert host.file("/app").exists
+    assert host.file("/app").user == "git"
+    assert host.file("/app").group == "git"
+    assert oct(host.file("/app").mode) == "0o550"
+
+
+def test_app_files(host):
+    assert host.file("/app/gogs").exists
+    assert host.file("/app/public").exists
+    assert host.file("/app/scripts").exists
+    assert host.file("/app/templates").exists
+
+
+def test_default_log_folder(host):
+    assert host.file("/app/log").exists
+    assert host.file("/app/log").user == "git"
+    assert host.file("/app/log").group == "git"
+    assert oct(host.file("/app/log").mode) == "0o770"
 
 
 def test_ssh_config(host):
-    host.file("/etc/ssh/sshd_config").exists
+    assert host.file("/etc/ssh/sshd_config").exists
+    assert host.file("/etc/ssh/sshd_config").user == "root"
 
 
 def test_nsswitch_config(host):
-    host.file("/etc/nsswitch.conf").exists
+    assert host.file("/etc/nsswitch.conf").exists
+    assert host.file("/etc/nsswitch.conf").user == "root"
 
 
-@pytest.mark.parametrize('package', [
-    ('bash'),
-    ('ca-certificates'),
-    ('linux-pam'),
-    ('openssh'),
-    ('shadow'),
-])
+@pytest.mark.parametrize(
+    "package",
+    [
+        ("bash"),
+        ("ca-certificates"),
+        ("linux-pam"),
+        ("openssh"),
+        ("shadow"),
+        ("s6-overlay"),
+    ],
+)
 def test_installed_dependencies(host, package):
     assert host.package(package).is_installed
